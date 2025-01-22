82dc15557578f03455d8d671f3c26e9633c22c94
newton.hoang <newtonhoang99@gmail.com>
2025-01-22 13:35:35 -0800

    Added catch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello idea!");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("What is your favorite number?");
+        //String name = scanner.nextLine();
+
+        Double num = 0d;
+
+        try{
+            num = scanner.nextDouble();
+        }catch(Exception e){
+            System.out.println(num + " is not a number.");
+            System.exit(0);
+        }
+        System.out.println("Cool! I like that favorite number is " + num);
+    }//End Constructor
+}//End Main
+//Newton Hoang
